42dde6440992fa18c934ce7af839645c7ad7037c
Maria Pinto Nogueira <64786262+nogueiram-27@users.noreply.github.com>
2021-02-10 20:48:19 +0000

    added Header component; added Link with react-router-dom to connect with the ShopPage component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import './App.css';
 
+import Header from './components/header/header.component';
 import Homepage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 
 function App() {
   return (
     <div >
+      <Header />
       <Switch>
         <Route exact={true} path='/' component={Homepage}/>
         <Route path='/shop/' component={ShopPage}/>
