0fec75d2e47c62e9c61de28131e0800cb2a5b2a3
Maria Pinto Nogueira <64786262+nogueiram-27@users.noreply.github.com>
2021-02-12 21:54:04 +0000

    added clear item and remove item actions to cart reducer; connect CheckoutItem component to these new actions

--- src/pages/checkout/checkout.component.js
+++ src/pages/checkout/checkout.component.js
@@ -3,7 +3,7 @@ import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect'
 import { selectCartItems , selectCartTotal } from '../../redux/cart/cart.selectors'
 
-import CheckoutItem from '../../components/checkout-item/checout-item.component'
+import CheckoutItem from '../../components/checkout-item/checkout-item.component'
 
 import './checkout.styles.scss'
 
--- src/redux/cart/cart.actions.js
+++ src/redux/cart/cart.actions.js
@@ -8,3 +8,13 @@ export const addItem = (item) => ({
     type: cartActionsType.ADD_ITEM,
     payload: item
 })
+
+export const clearItem = (item) => ({
+    type: cartActionsType.CLEAR_ITEM_FROM_CART,
+    payload: item
+})
+
+export const removeItem = (item) => ({
+    type: cartActionsType.REMOVE_ITEM,
+    payload: item
+})
