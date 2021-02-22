696425cf2c5ba308806c5158b61c60039bbd79c6
Maria Pinto Nogueira <64786262+nogueiram-27@users.noreply.github.com>
2021-02-22 14:40:05 +0000

    fixed error in express server; added folders to gitignore

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 if(process.env.NODE_ENV === 'production') {
-    app.use(express.static(join(__dirname, 'client/build')));
+    app.use(express.static(path.join(__dirname, 'client/build')));
     app.get('*', function(req, res) {
         res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
     });
